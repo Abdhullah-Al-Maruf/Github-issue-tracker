10b6e0de8c659506b66894c555b0747396f04c4f
Abdhullah-Al-Maruf <mdmaruf20053@gmail.com>
2026-03-09 11:05:32 +0600

    btn-toogling added

--- script/homepage.js
+++ script/homepage.js
@@ -1,1 +1,27 @@
-console.log("homepage is loading");
+// 1 btn toggling
+  const tabs= document.querySelectorAll(".tab-btn")
+    tabs.forEach(tab => {
+        // add each btn a listener
+        tab.addEventListener("click",()=>{
+            // remove the color from all tab at a time
+            tabs.forEach(btn =>{
+                btn.classList.remove("btn-primary")
+                btn.classList.remove("btn-soft")
+
+            } );
+            //add color to the only one btn
+            tab.classList.add("btn-primary")
+        })
+    });
+
+
+// 2 card section
+
+
+
+
+
+
+
+
+
